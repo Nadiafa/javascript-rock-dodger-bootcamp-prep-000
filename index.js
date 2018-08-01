50a3a964bb7c14b578524485fba924565b434d66
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-01 08:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -108,7 +108,8 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  clearInterval(gameInterval);
+  clearInterval(gameInterval
+  ROCKS.forEach(function(rock) { rock.remove() })
 }
 
 
