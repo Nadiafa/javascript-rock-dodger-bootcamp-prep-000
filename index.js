203b43f8ee47fc345536bf6f3febaeedc16e050d
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-01 08:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-  if (e.which !== LEFT_ARROW || e.which !== RIGHT_ARROW) {
+  if (e.which !== LEFT_ARROW && e.which !== RIGHT_ARROW) {
     e.preventDefault()
     e.stopPropagation()
   }
