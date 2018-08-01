d8995090c64b635a70e9d24342a170a3f789a45c
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-01 08:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -110,7 +110,8 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock) { rock.remove() });
-  
+  document.removeEventListener('keydown', moveDodger);
+  return alert("YOU LOSE!");
 }
 
 
